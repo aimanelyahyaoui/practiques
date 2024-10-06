32be4e720ac7e47547727c617c58e0e87707b0da
aimanelyahyaoui <1749573@uab.cat>
2024-10-06 21:37:24 +0200

    Update codi.sh
    Implementació del segon exercici (prova)

--- codi.sh
+++ codi.sh
@@ -3,4 +3,6 @@
 cd pratiques
 awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 
-~/practiques$ cut -d ',' -f '1-2' supervivents.csv
+~/practiques$ cut -d ',' --complement -f 12,16 supervivents.csv > sortida.csv
+
+~/practiques$ awk -F ',' '$15 != "True" { print $0 }' sortida.csv 
