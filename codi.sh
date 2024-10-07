19b23e38d555575ca4c2e40db425c4974b41c043
aimanelyahyaoui <1749573@uab.cat>
2024-10-07 09:02:29 +0200

    Update codi.sh

--- codi.sh
+++ codi.sh
@@ -3,6 +3,10 @@
 cd pratiques
 awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 
+#exercici 1#
 ~/practiques$ cut -d ',' --complement -f 12,16 supervivents.csv > sortida.csv
 
-~/practiques$ awk -F ',' '$15 != "True" { print $0 }' sortida.csv 
+#execici 2#
+~/practiques$ awk -F ',' '$15 != "True" { print $0 }' sortida.csv > sortida2.csv
+~/practiques$ wc (sortida.csv - sortida2.csv) = registres_elimnats
+~/practiques$ echo registres_eliminats
