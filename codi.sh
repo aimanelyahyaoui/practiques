11d4b97ef305109dd1457e5ba8591db6dd027106
aimanelyahyaoui <1749573@uab.cat>
2024-10-07 10:29:17 +0200

    Update codi.sh
    add exercici3

--- codi.sh
+++ codi.sh
@@ -8,7 +8,12 @@ awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 
 #execici 2#
 ~/practiques$ awk -F ',' '$14 != "True"' sortida.csv > sortida2.csv
-entrada = (wc -l < sortida.csv)
-sortida = (wc -l < sortida2.csv)
-~/practiques$ wc (entrada - sortida)
 
+~/practiques$ echo $(($(wc -l < sortida.csv) - $(wc -l < sortida2.csv)))
+
+#exercici 3#
+~/practiques$ awk -F,  '{ views = $8 + 0; 
+  if(views <= 1000000) {ranking = "Bo";} 
+  else if (views<=10000000 && views>1000000) {ranking = "Ecxel·lent";} 
+  else {ranking = "Estrella";} 
+print $0 "," ranking;}' sortida2.csv > sortida3.csv
