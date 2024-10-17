debc1eced1551c7e493c3d0a6cb8d0749b6e00c0
aimanelyahyaoui <1749573@uab.cat>
2024-10-17 08:54:22 +0200

    Update codi.sh

--- codi.sh
+++ codi.sh
@@ -4,15 +4,18 @@ cd pratiques
 awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 
 #exercici 1#
-~/practiques$ cut -d ',' --complement -f 12,16 supervivents.csv > sortida.csv -r
+~/practiques$ cut -d ',' --complement -f 12,16 supervivents.csv > sortida.csv
 
 #execici 2#
-~/practiques$ awk -F ',' '$14 != "True"' sortida.csv > sortida2.csv
+~/practiques$ awk -F ',' '$14 != "True"' sortida.csv > sortida2.csv && echo $(($(wc -l < sortida.csv) - $(wc -l < sortida2.csv)))
 
-~/practiques$ echo $(($(wc -l < sortida.csv) - $(wc -l < sortida2.csv)))
 
 #exercici 3#
-~/practiques$ awk -F,  '{ views = $8 + 0; if(views <= 1000000) {ranking = "Bo";}  else if (views<=10000000 && views>1000000) {ranking = "Excel·lent";}  else {ranking = "Estrella";} print $0 "," ranking;}' sortida2.csv > sortida3.csv
+~/practiques$ awk -F,  '{ views = $8 + 0; 
+    if(views <= 1000000) {ranking = "Bo";}  
+    else if (views<=10000000 && views>1000000) {ranking = "Excel·lent";}  
+    else {ranking = "Estrella";} print $0 "," ranking;}' 
+    sortida2.csv > sortida3.csv
 
 #exercici 4.0#
 # Escriure la capçalera amb les noves columnes Rlikes i Rdislikes
